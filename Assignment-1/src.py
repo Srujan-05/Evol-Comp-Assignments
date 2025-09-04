ea9044646bd397bd7bacaa7f4ebf95c6cd7f1159
Srujan-05 <srujantallam@gmail.com>
2025-09-04 21:35:52 +0530

    Classes added

--- Assignment-1/src.py
+++ Assignment-1/src.py
@@ -1,1 +1,48 @@
 # implementation of all the DE Algorithm related classes.
+import numpy as np
+
+
+class DifferentialEvolution:
+    def __init__(self, population_size, no_of_gens, no_design_vars, fitness_func, constraints, crossover_prob, K, optimisation_option):
+        self.popl_size = population_size
+        self.number_gens = no_of_gens
+        self.num_des_vars = no_design_vars
+        self.fitness = fitness_func
+        self.constraints = constraints
+        self.crossover_prob = crossover_prob
+        self.K = K
+        self.opt_op = optimisation_option
+
+    def mutantVectorGeneration(self):
+        pass
+
+    def trialVectorGeneration(self):
+        pass
+
+    def run(self):
+        pass
+
+
+class Candidates:
+    def __init__(self, num_design_vars):
+        self.vector = np.zeros(shape=(num_design_vars, 1))
+        self.best = None
+        self.best_vec = np.zeros(shape=self.vector.shape)
+
+
+class Constraints:
+    def __init__(self, constraint, type):
+        """
+        :param constraint: the constraint to be supplied as a function of the design variables with the constraint
+                            operation against 0
+        :param type: ['<', '>', '<=', '>=', '!='] are the only arguments to be accepted. this
+                    tells us what type of constraint we are applying.
+        example:
+            if the applied constraint on the problem is x < 100, the class initialisation is as:
+                constrain = lambda x: x-100 (or) any function that does the similar operation
+                type = '<'
+            the function then compares it to 0 and sees if the function values are actually valid.
+        """
+        self.constraint = constraint
+        self.type = type
+
--- Assignment-1/utils.py
+++ Assignment-1/utils.py
@@ -1,1 +1,13 @@
 # implementation of the results visualization class.
+class FitnessFunc:
+    def __init__(self, fitness_function):
+        """
+        :param fitness_function: the fitness function in terms of all the design variables.
+                                Can be a lambda function or a simple function.
+        """
+        self.fit_func = fitness_function
+
+
+class Visualization:
+    """Sorry couldn't figure out what to do here yet. Abhishek try and tinker and let me know"""
+    pass
