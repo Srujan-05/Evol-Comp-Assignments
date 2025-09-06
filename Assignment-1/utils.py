4e7f3415fd6759d7cb50ca6668786ebdd4641f70
Srujan-05 <srujantallam@gmail.com>
2025-09-06 10:05:42 +0530

    Moved fitness function class to src.py

--- Assignment-1/utils.py
+++ Assignment-1/utils.py
@@ -1,11 +1,4 @@
 # implementation of the results visualization class.
-class FitnessFunc:
-    def __init__(self, fitness_function):
-        """
-        :param fitness_function: the fitness function in terms of all the design variables.
-                                Can be a lambda function or a simple function.
-        """
-        self.fit_func = fitness_function
 
 
 class Visualization:
