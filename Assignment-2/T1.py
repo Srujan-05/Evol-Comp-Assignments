9f81c83819d8a6a47bb76e82f1f86643ed1fc1c8
Srujan-05 <srujantallam@gmail.com>
2025-10-07 18:40:26 +0530

    Completed Fuzzification and Normalizaton classes

--- Assignment-2/T1.py
+++ Assignment-2/T1.py
@@ -7,10 +7,39 @@ class FuzzyMap:
         self.bins = len(centroids) if bins is None else bins
         self.centroids = centroids if centroids is not None else np.linspace(-1, 1, bins)
 
+    def mu(self, x: int) -> np.array:
+        belonging = [0 for i in range(self.bins)]
+        if x <= self.centroids[0]:
+            belonging[0] = 1/(self.centroids[0] + 1) * (x + 1)
+            belonging = [1-belonging[0]] + belonging + [0]
+        elif x >= self.centroids[-1]:
+            belonging[-1] = 1 / (self.centroids[-1] - 1) * (x - 1)
+            belonging = [0] + belonging + [1 - belonging[-1]]
+        else:
+            for i in range(1, self.bins):
+                if self.centroids[i - 1] <= x <= self.centroids[i]:
+                    belonging[i] = 1 / (self.centroids[i] - self.centroids[i-1]) * (x - self.centroids[i-1])
+                    belonging[i-1] = 1 - belonging[i]
+                    break
+
+            belonging = [0] + belonging + [0]
+
+        return np.array(belonging)
+
+    def defuzzify(self, belong: np.array):
+        return (belong[0]*-1 + np.dot(belong[1:-1], self.centroids) + belong[-1] * 1)/np.sum(belong)
+
 
 class Normalization:
-    def __init__(self, input: np.array):
-        self.input = input
+    def __init__(self, minLim: int, maxLim: int):
+        self.minVal = minLim
+        self.maxVal = maxLim
+
+    def normalize(self, x):
+        return (2*x - (self.maxVal + self.minVal))/(self.maxVal-self.minVal)
+
+    def denormalize(self, d):
+        return (d * (self.maxVal - self.minVal) + (self.maxVal + self.minVal)) / 2
 
 
 class FuzzyLogic:
@@ -22,4 +51,14 @@ class FuzzyLogic:
 
 
 if __name__ == "__main__":
-    pass
+    fm = FuzzyMap(None, np.array([-0.66, -0.33, 0, 0.15, 0.33, 0.45, 0.75]))
+    belong = fm.mu(1)
+    defuz = fm.defuzzify(belong)
+    print(belong, defuz)
+
+    nm = Normalization(-30, 30)
+    normVal = nm.normalize(0.1)
+    print("normalized: ", normVal)
+    denormVal = nm.denormalize(normVal)
+    print("denormalized: ", denormVal)
+
