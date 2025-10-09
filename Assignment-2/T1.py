c169a60dd4017905d21ad16590d05e7763bb5047
Alekya <alekya.duvvuri17@gmail.com>
2025-10-09 23:27:03 +0530

    Written the first version of def solve under class FuzzyLogic. Changes to be made-if found needed

--- Assignment-2/T1.py
+++ Assignment-2/T1.py
@@ -42,12 +42,49 @@ class FuzzyLogic:
     def __init__(self, input_data: np.array, input_ranges: list, input_fuzzy_maps: list, output_fuzzy_map: FuzzyMap, fam: np.array):
         self.input_data = input_data
         self.input_fuzzy_maps = input_fuzzy_maps
-        self.output_fuzzy_maps = output_fuzzy_map
+        self.output_fuzzy_map = output_fuzzy_map
         self.fam = fam
         self.input_ranges = input_ranges
+        self.normalizers = [Normalization(range[0], range[1]) for range in input_ranges]
 
     def solve(self):
-        pass
+        normalized_outputs = []
+        input_list = self.input_data
+
+        for input_data in input_list:
+            normalized_inputs = []
+            for i, data in enumerate(input_data):
+                norm_data = self.normalizers[i].normalize(data)
+                normalized_inputs.append(norm_data)
+            
+            input_memberships = []
+            for i, norm_input in enumerate(normalized_inputs):
+                fuzzy_val = self.input_fuzzy_maps[i].mu(norm_input)
+                input_memberships.append(fuzzy_val)
+
+            firing_intensities = np.zeros_like(self.fam, dtype = float)
+
+            for i in range(len(input_memberships[0])):
+                for j in range(len(input_memberships[1])):
+                    firing_intensities[j, i] = input_memberships[0][i] * input_memberships[1][j]
+
+            numerator, denominator = 0.0, 0.0
+
+            for i in range(firing_intensities.shape[0]):
+                for j in range(firing_intensities.shape[1]):
+                    firing_strength = firing_intensities[i, j]
+                    output_set_index = self.fam[i, j] - 1
+
+                    if output_set_index < len(self.output_fuzzy_map.centroids) and firing_strength > 0:
+                        centroid = self.output_fuzzy_map.centroids[output_set_index]
+                        numerator += firing_strength * centroid
+                        denominator += firing_strength
+
+            normalized_output = numerator / denominator if denominator != 0 else 0.0
+            normalized_outputs.append(normalized_output)
+
+        return normalized_outputs
+
 
 
 if __name__ == "__main__":
